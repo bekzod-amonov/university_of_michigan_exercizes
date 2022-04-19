d1ae4e6ac367068e4bc906388a64cef6440abcf1
bekzod-amonov <99309062+bekzod-amonov@users.noreply.github.com>
2022-04-19 07:31:32 +0500

    Add files via upload

---
+++ ex_09_04.py
@@ -0,0 +1,20 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+counts = dict()
+lst = list()
+largestnum = None
+largestval = None
+for word in fh:
+    if word.startswith('From '):
+        words = word.split()
+        nwords = words[1]
+        lst.append(nwords)
+
+for element in lst:
+    counts[element] = counts.get(element,0) + 1
+
+for key,value in counts.items():
+    if largestnum is None or value > largestnum:
+        largestnum = value
+        largestval = key
+print(largestval, largestnum)
