5a5fbddefcb66c160b5c71b8c0691816f4fbf322
bekzod-amonov <99309062+bekzod-amonov@users.noreply.github.com>
2022-04-28 16:49:42 +0500

    Add files via upload

---
+++ basic_xml_2.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+input = '''
+<stuf>
+    <users>
+        <user x ='2'>
+            <id>001</id>
+            <name>Bekzod</name>
+        </user>
+        <user x='7'>
+            <id>009</id>
+            <name>Sherzod</name>
+        </user>
+    </users>
+</stuf> '''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+    print('Name', item.find('name').text)
+    print('ID', item.find('id').text)
+    print('Attribute', item.get('x'))
